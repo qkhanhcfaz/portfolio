9b17f35e98825f237b1d2e9cf24426f409b9ab0f
qkhanhcfaz <tranvanquockhanh123@gmail.com>
2023-09-14 06:17:03 +0700

    change code

--- assets/js/main.js
+++ assets/js/main.js
@@ -88,4 +88,58 @@ const today = new Date();
 //show date
 document.getElementById("date").innerHTML = `${today.getDate()}-${today.getMonth() + 1}-${today.getFullYear()}`;
 
+//show in fo toast 
+function showInfoToast() {
+    toast({
+        title: "Notification!",
+        message: "This feature will be updated soon",
+        type: "info",
+        duration: 5000
+    });
+}
+//new code
+// handle showInfoToast
+// toast function
+function toast({ title = "", message= "", type="info", duration = 3000 }) {
+    const main = document.getElementById("toast");
+    if(main) {
+        const toast = document.createElement("div");
+    
 
+    // Auto remove toast
+    const autoRemoveId = setTimeout(function () {
+        main.removeChild(toast);
+    }, duration + 1000);
+
+    // Remove toast when clicked
+    toast.onclick = function (e) {
+        if(e.target.closest(".toast__close")) {
+            main.removeChild(toast);
+            clearTimeout(autoRemoveId);
+        }
+    };
+
+    const icons = {
+        info: "fas fa-info-circle"
+    };
+    const icon = icons[type];
+    const delay = (duration / 1000). toFixed(2);
+
+    toast.classList.add("toast", `toast--${type}`);
+    toast.style.animation = `slideInLeft ease .3s, fadeOut line 1s${delay}s forwards`;
+
+    toast.innerHTML = ` 
+                <div class="toast__icon">
+                    <i class="${icon}"></i>
+                </div>
+                <div class="toast__body">
+                    <h3 class="toast__title">${title}</h3>
+                    <p class="toast__msg">${message}</p>
+                </div>
+                <div class="toast__close">
+                    <i class="fas fa-times"></i>
+                </div>
+                      `;
+    main.qppendChild(toast);          
+}
+}
